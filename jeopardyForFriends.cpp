cde2876cb9c20c51d4c146ae81c04b5437612475
PolarBearInGethub <polardog828@gmail.com>
2026-07-08 17:58:57 -0700

    Barely Started
    So, I was just amidst doing my own little project until I realized I forgot to do my other work!! AAAAA

---
+++ jeopardyForFriends.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<std::string> player_list;
+    int player_amount;
+    std::string player_name;
+    std::cout << "Welcome to my Jeopardy game!" << std::endl;
+    std::cout << "How many are playing?" << std::endl;
+    std::cin >> player_amount;
+    for (int i = 0; i < player_amount; i++) {
+        
+    }
+    return 0;
+}
